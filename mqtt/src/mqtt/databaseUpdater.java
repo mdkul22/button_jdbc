271720c2e96fe74c5b99acc0b4f35d7353696a1f
mdkul22 <mdkul22@gmail.com>
2018-06-22 13:32:18 +0530

    making the database handler

---
+++ mqtt/src/mqtt/databaseUpdater.java
@@ -0,0 +1,66 @@
+package mqtt;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
+
+public class databaseUpdater implements MqttCallback{
+	MqttClient client;
+
+
+	public databaseUpdater() {	
+	}
+
+	public static void main(String[] args)
+	{
+		new databaseUpdater().beginSubscribe();
+	}
+
+	public void beginSubscribe() {
+		try{
+			client = new MqttClient("tcp://173.39.91.82:1883", "Sending");
+			client.connect();
+			client.setCallback(this);
+			client.subscribe("tqb/topic");
+			client.subscribe("tqb/alert");
+			MqttMessage message = new MqttMessage();
+			message.setPayload("Lets start this stuff!".getBytes());
+			client.publish("tqb/begin", message);
+		}
+		catch(MqttException e){
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void connectionLost(Throwable cause){
+	System.out.println("Connection lost!");	
+	}
+
+	@Override 
+	public void messageArrived(String topic, MqttMessage message) 
+			throws Exception {
+		System.out.println(message);
+		byte obj[] = message.getPayload();
+		String s = new String(obj);
+		insertQuery update = new insertQuery(s);
+		if(topic.equals("tqb/topic"))
+		{
+			
+		}
+		if(topic.equals("tqb/setup")){
+			
+		}
+
+		
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken token)
+	{	
+		System.out.println("message delivery done!");
+	}
+
+
+}
--- mqtt/src/mqtt/insertQuery.java
+++ mqtt/src/mqtt/insertQuery.java
@@ -1,65 +1,115 @@
 package mqtt;
-import org.json.*
-import java.sql.*
+import org.json.*;
+import java.util.*;
+import java.sql.*;
 
 public class insertQuery {
 static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 static final String DB_URL = "jdbc:mysql://localhost/button_data?useSSL=false";
 static final String USER = "root";
-static final String USER = "root";
-private String table;
+static final String PASS = "root";
+static private String row;
 
-public insertQuery(String table) {
-table = table;
+public insertQuery(String x) {
+row = x;
 }
 
-public static void main(String[] args)
-{
-	new Listener(table).insertRow();
-}
-
-public void insertRow() {
+public void insertSetupRow() {
+	Connection conn = null;
+	Statement stmt =  null;
+	JSONObject jsonObj = new JSONObject(row);
+	String mac = jsonObj.getString("mac");
+	String building = jsonObj.getString("building");
+	String floor = jsonObj.getString("floor");
+	String posX = jsonObj.getString("posX");
+	String posY = jsonObj.getString("posY");
 	try{
- Class.forName("com.mysql.jdbc.Driver");
-System.out.println("Connecting to the database");
-conn = DriverManager.getConnection(DB_URL, USER, PASS);
-stmt = conn.createStatement();
-System.out.println("Creating statement..");
-String sql;
-sql = "select mac, building, floor, posX, posY from tqb_setup";
-ResultSet rs = stmt.executeQuery(sql);
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Connecting to the database");
+		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		stmt = conn.createStatement();
+		System.out.println("Creating statement..");	
+		String sql;
+		sql = "INSERT INTO tqb_setup "   ;
+        sql += "(mac, building, floor, posX, posY) ";
+        sql += "VALUES ";
+        sql += "(" + mac + ", " + building + ", ";
+        sql += floor + ", " + posX + ", " + posY + ");";
+		ResultSet rs = stmt.executeQuery(sql);
+		System.out.println("inserted row!");
+		rs.close();
+		stmt.close();
+		conn.close();
+		}
+	catch(SQLException se){
+		se.printStackTrace();
+		}
+	catch(Exception e){
+		e.printStackTrace();
+		}
+	finally{
+		try{
+			if(conn!=null)
+				conn.close();
+		}
+		catch(SQLException se){
+			se.printStackTrace();
+		}
+	}
+System.out.println("Exit insert setup method!");
+}
 
-while(rs.next()){
-String mac = rs.getString("mac");
-String building = rs.getString("building");
-String floor = rs.getString("floor");
-String posX = rs.getString("posX");
-String posY = rs.getString("posY");
-System.out.print("mac: " + mac);
-System.out.print("building: " + building);
+public void insertAlertRow() {
+	Connection conn = null;
+	Statement stmt =  null;
+	JSONObject jsonObj = new JSONObject(row);
+	String mac = jsonObj.getString("mac");
+	// getting keys of json
+    Set<String> keys = jsonObj.keySet();	
+    Iterator itr = keys.iterator();
+    String[] val = new String[2];
+    int i = 0;
+    String alertkey;
+    do{
+        val[i] = itr.next().toString();
+        System.out.println(val[i]);
+        i++;
+
+    }while(itr.hasNext());
+    alertkey = val[1];
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Connecting to the database");
+		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		stmt = conn.createStatement();
+		System.out.println("Creating statement..");	
+		String sql;
+		sql = "INSERT INTO tqb_alert "   ;
+        sql += "(mac, alert) ";
+        sql += "VALUES ";
+        sql += "(" + mac + ", " + alertkey + ");";
+		ResultSet rs = stmt.executeQuery(sql);
+		System.out.println("inserted row!");
+		rs.close();
+		stmt.close();
+		conn.close();
+		}
+	catch(SQLException se){
+		se.printStackTrace();
+		}
+	catch(Exception e){
+		e.printStackTrace();
+		}
+	finally{
+		try{
+			if(conn!=null)
+				conn.close();
+		}
+		catch(SQLException se){
+			se.printStackTrace();
+		}
+	}
+System.out.println("Exit insert alert method!");
 }
-System.out.println("Using Listener!");
-rs.close();
-stmt.close();
-conn.close();
-Listener sub = new Listener();
-sub.doDemo();
-}
-catch(SQLException se){
-se.printStackTrace();
-}
-catch(Exception e){
-e.printStackTrace();
-}
-finally{
-try{
-if(conn!=null)
-conn.close();
-}
-catch(SQLException se){
-se.printStackTrace();
-}
-}
-System.out.println("BYE");
-}
+
 }
